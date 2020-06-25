9a951d8b4adbd0e3c10b7f018b886ba04211d873
tienthebell <67412776+tienthebell@users.noreply.github.com>
2020-06-25 16:33:32 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://bit.ly/2r37SFT';
+	var url_popup = 'https://www.facebook.com/Batdongsangiatotvensaigon/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
